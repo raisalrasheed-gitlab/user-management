25ae1b0fc4ea94e26fa3605d7e3cc3d299053705
Raisal Rasheed <raisalrasheed545@gmail.com>
2025-02-21 06:53:24 +0530

    list of all users

--- src/routes/v1/userRoutes.js
+++ src/routes/v1/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
-const { addUser, deleteUser } = require('../../controllers/user-control');
+const {
+  addUser,
+  deleteUser,
+  getAllUsers,
+} = require('../../controllers/user-control');
 const router = express.Router();
 
+router.get('/', getAllUsers);
 router.post('/', addUser);
 router.delete('/:id', deleteUser);
 
